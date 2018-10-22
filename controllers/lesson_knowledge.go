b1c8672a536c7bcc2b1406276dfcf9d86b736a91
luoqing <luoqing@jiedaibao.com>
2018-10-22 16:52:51 +0800

    some unimportant changes

--- controllers/lesson_knowledge.go
+++ controllers/lesson_knowledge.go
@@ -11,7 +11,6 @@ type LessonKnowledgeController struct {
 
 func (lk *LessonKnowledgeController) Get() {
 	lesson_id, _ := lk.GetInt("lesson_id")
-	// lk.Ctx.WriteString("get lesson Knowledge")
 	o := orm.NewOrm()
 	var maps []orm.Params
 	o.QueryTable("mp_english_less_knowledge").Filter("lesson_id", lesson_id).Values(&maps)
